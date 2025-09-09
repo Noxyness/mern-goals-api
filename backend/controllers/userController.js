82b74adf0d3c6009415fd66e2f8e799791e71818
Jean Cloude Claus <engr.jclaus@gmail.com>
2025-09-09 13:26:26 +0800

    added a loginuser with compare

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if(user) {
         res.status(201).json({
-            _id: user.id,
+            _id: user._id,
             name: user.name,
             email: user.email
         })
@@ -53,7 +53,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // POST /api/users/login
 // public
 const loginUser = asyncHandler(async (req,res) =>{
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+    //check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error ('Invalid credentials')
+    }
 })
 // Get user data
 // GET /api/users/me
